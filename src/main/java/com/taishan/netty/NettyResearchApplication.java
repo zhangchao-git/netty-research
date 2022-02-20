b01a037c3642bcf92b05344d4ffc797d62390a05
zhang <1045685272@qq.com>
2022-02-20 22:07:20 +0800

    Initial commit

---
+++ src/main/java/com/taishan/netty/NettyResearchApplication.java
@@ -0,0 +1,13 @@
+package com.taishan.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyResearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyResearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taishan/netty/NettyResearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taishan.netty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyResearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
