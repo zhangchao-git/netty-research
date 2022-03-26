a99b08fe26ab08f475d4ea967040acf16feafb7f
zhang <1045685272@qq.com>
2022-03-26 20:57:40 +0800

    数据库密码加密

--- src/test/java/com/taishan/iot/NettyResearchApplicationTests.java
+++ src/test/java/com/taishan/iot/NettyResearchApplicationTests.java
@@ -3,6 +3,7 @@ package com.taishan.iot;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
 import io.netty.buffer.ByteBufUtil;
+import org.jasypt.util.text.BasicTextEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -52,4 +53,16 @@ class NettyResearchApplicationTests {
         System.out.print(ByteBufUtil.hexDump(bb));
     }
 
+
+    @Test
+    public void encryptPwd() {
+        BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+        //加密所需的salt
+        textEncryptor.setPassword("998877");
+        //要加密的数据（数据库的用户名或密码）
+        String username = textEncryptor.encrypt("root");
+        String password = textEncryptor.encrypt("123");
+        System.out.println("username:" + username);
+        System.out.println("password:" + password);
+    }
 }
